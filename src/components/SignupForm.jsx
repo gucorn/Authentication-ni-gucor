8cbdfc9c3cc4e60859f4344bcce8e89a30641492
KateBaul <katedanica.baul@gmal.com>
2024-04-12 16:45:04 +0800

    Authentication

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -46,16 +46,9 @@ function SignupForm() {
 
   return (
     <div className="signup-container" style={{ borderColor: "#5AB1FF" }}>
-      <div className="logo-container">
-        <a href="#">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="#">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       {/* Form */}
-      <h2 style={{ color: "#5AB1FF" }}>Rehistro</h2>
+      <h2 style={{ color: "#5AB1FF" }}>Sign Up</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group email">
           <label htmlFor="email" className="text-primary">
